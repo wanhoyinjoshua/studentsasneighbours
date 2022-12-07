34901f257819704a2b9b4ac2e75be6d2494ce966
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2022-12-07 19:53:13 +1100

    hi

--- pages/error_404.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const error_404 = () => {
-  return (
-    <div>It looks like the form was not sent successfully, try refreshing the browser and make sure you ahve good internet connection!</div>
-  )
-}
-
-export default error_404
